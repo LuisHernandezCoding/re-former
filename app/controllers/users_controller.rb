09f16aeca5c32bc20d412093417f688c6c2e5a03
LuisHernandezCoding <LuisHernandezCoding@gmail.com>
2022-11-30 20:56:17 -0600

    - Added Railsy way to form at new.html.erb - Filled #new and #create actions in UsersController - Fix on User model validations (typo)

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,22 @@
 class UsersController < ApplicationController
-  def new; end
+  def new
+    @user = User.new
+  end
 
-  def create; end
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      flash[:success] = 'Welcome to the Sample App!'
+      redirect_to new_user_path
+    else
+      flash[:danger] = 'Invalid email/password combination'
+      render :new, status: :unprocessable_entity, locals: { user: @user }
+    end
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:username, :email, :password)
+  end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,7 @@
 class User < ApplicationRecord
-  validates :name, :email, :password, presence: true
-  validates :name, :email, uniqueness: true
-  validates :name, length: { maximum: 32 }
+  validates :username, :email, :password, presence: true
+  validates :username, :email, uniqueness: true
+  validates :username, length: { maximum: 32 }
   validates :email, length: { maximum: 64 }
   validates :password, length: { maximum: 64 }
 end
